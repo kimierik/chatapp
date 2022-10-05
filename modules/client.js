1a7b86854a9209df5a36e6bc6e9a9439904c1d8a
kimierik <kimi.malkamaki@gmail.com>
2022-10-05 19:12:45 +0300

    autoscrol to new msg

--- modules/client.js
+++ modules/client.js
@@ -1,23 +1,37 @@
+var socket=io();
+var inp=document.getElementById('buddon');
+var txtinput=document.getElementById('txtibnpu');
+var board=document.getElementById('messege_field');
+var messegearea=document.getElementById("msgarea")
+inp.addEventListener('click',func);
+txtinput.addEventListener('submit',func);
+
+let isbottom=false;
+function scroll(){
+	if (isbottom){
+	messegearea.scrollTo(0,messegearea.scrollHeight)
+	}
+}
+
+function func(){
+	socket.emit('msg',txtinput.value);
+}
 
 
 
-	var socket=io();
-	var inp=document.getElementById('buddon');
-	var txtinput=document.getElementById('txtibnpu');
-	var board=document.getElementById('messege_field');
-	inp.addEventListener('click',func);
 
-	function func(){
-		socket.emit('msg',txtinput.value);
+socket.on('msg',function(data){
+	if (messegearea.scrollHeight==messegearea.scrollTop+500){
+		isbottom=true;
+	}else{
+		isbottom=false;
 	}
 
-	socket.on('msg',function(data){
-		var item=document.createElement('p');
-		item.textContent=data;
-		board.appendChild(item);
-
-
-	});
+	var item=document.createElement('p');
+	item.textContent=data;
+	board.appendChild(item);
+	scroll();
+});
 
 
 
